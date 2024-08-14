e075036524291e6b07a121a69f6b9a0a251ee91b
Evan <158116844+Gnauk841026@users.noreply.github.com>
2024-08-14 16:31:15 +0800

    Update app.js

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Cloud Run! Test Alert 2');
+  res.send('Hello from Cloud Run! Test Alert 10');
 });
 
 app.listen(port, () => {
